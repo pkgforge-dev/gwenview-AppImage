07f00c0cc9a18163a3a0c5b520b36f96b87fd541
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-02 12:01:07 -0400

    remove `qt6-webengine`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,6 +12,9 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
+# remove qt6webengine, otherwise it gets deployed due to it having a qml plugin
+pacman -Rdd --noconfirm qt6-webengine
+
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
 
